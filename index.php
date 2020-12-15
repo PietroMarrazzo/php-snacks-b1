a6ce14148b02c04748a5c3634deba7301e288a62
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-12-15 16:13:12 +0100

    fixed

--- index.php
+++ index.php
@@ -1,34 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snack01</title>
-</head>
-<body>
+
+<?php
+        // ini_set('display_errors','On');
+        // error_reporting(E_ALL);
+
+        $name = $_GET['name'];
+        $mail = $_GET['mail'];
+        $age = $_GET['age'];
+
+        // $info = [
+        //     'name' => $name;
+        //     'mail' => $mail;
+        //     'age' => $age;
+        // ]
     
-
-
-    <?php
-        $name = $_GET('name');
-        $mail = $_GET('mail');
-        $age = $_GET('age');
-
-        $info = [
-            'name' => $name;
-            'mail' => $mail;
-            'age' => $age;
-        ]
-
-
+        var_dump($age);
         // ciclo verifica
         if (strlen($name) < 3 ) {
             echo 'Inserisci un nome più lungo di 3 caratteri';
-        } elseif ( strpos($mail, '@') === false || (strpos($mail, '.') === false )  {
+        } elseif ( strpos($mail, '@') === false || (strpos($mail, '.') === false ) )  {
             echo 'Inserisci una mail corretta';
         } elseif (is_numeric($age) === false) {
             echo 'Inserisci un\'età numerica';
-        }
-    ?>
-</body>
-</html>
+        } 
+?>
